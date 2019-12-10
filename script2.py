12fdc337a548846975c436faba86bc1d5f672b88
Antia Fraga Lourido <antia.fraga.lourido@udc.es>
2019-12-10 17:43:42 +0100

    Added steps to do in script 2, modified script 2

--- script2.py
+++ script2.py
@@ -4,19 +4,22 @@ import matplotlib.pyplot as plt
 %matplotlib inline
 
 from sklearn.model_selection import train_test_split
-from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import SVC
 from sklearn.svm import LinearSVC
 
 #Leemos los datos de los dos ficheros
 datos = pd.read_csv("~/datos.csv")
 output = pd.read_csv("~/output.csv")
-datos_train, datos_test, output_train, ouput_test = train_test_split(datos, output, test_size = 0.20)
-# Crear 5 modelos para cada uno de los targets
-# Cada modelo usa los mismos datos, es de tipo "es X, es no X"
+#partir los datos tanto las entradas como los targets de forma aleatoria pero consistente
+datos_train, datos_test, output_train, ouput_test = train_test_split(datos, output, test_size = 0.20, random_state = 42)
+#el interés en poner nuestra propia semilla es la de aleatorizar siempre de la misma manera los datos.
 
+for x in xrange(1,10000):
+	#inicializar el modelo
+	svclassifier = SVC(kernel='linear')
+	#entrenar el modelo
+	training = svclassifier.fit(datos_train)
 
-OneVsRestClassifier(LinearSVC(random_state=0)).fit(datos_train, output_train)
+	test = svclassifier.predict(datos_test)
+	lista.append(test.metrics.accuracy)
 
-svclassifier = SVC(kernel='linear')
-svclassifier.fit(datos_train)
